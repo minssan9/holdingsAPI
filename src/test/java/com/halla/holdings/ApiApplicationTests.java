774a0e0e46e78c9b56b342a236cb25cd77812c2c
Sanghun Min <minssan9@gmail.com>
2020-12-03 14:16:01 +0900

    initial commit

---
+++ src/test/java/com/halla/holdings/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halla.holdings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
